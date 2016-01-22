9e3f422d98e6bc10c289380a6f8ea4d53f10a1ea
Max Bullo <koza4ek90@gmail.com>
2016-01-23 00:31:18 +0200

    updated lessons 7-8 form js

--- GoIT/javascript/lessons 7-8/form/js/script.js
+++ GoIT/javascript/lessons 7-8/form/js/script.js
@@ -1,30 +1,26 @@
 $(function () {
-   var arr = ['<span class="hint">Please provide your firstname</span>', 
-              '<span class="hint">Please provide also your lastname</span>', 
-              '<span class="hint">Your  home or work  address</span>',
-              '<span class="hint hint--submit">Please fill in all fields</span>'];
-
    var $takeHint = $('.text');
    $takeHint.hover(hintIn, hintOut);
    function	hintIn() {
+      $('.hint').remove();
       var index = $(this).parent().index();
-      $('.field').eq(index).append(arr[index]);
+      var title = $(this).attr('title');      
+      $('.field').eq(index).append('<span class="hint">'+title+'</span>');
+      $(this).focusin(hintOut);
    }
    function hintOut() {
    	  $('.hint').remove();
    }
 
    var $submit = $('.submit');
-   $submit.hover(submitIn, submitOut);
+   $submit.click(submitIn);
    function submitIn() {
-   	   if ($takeHint.eq(0).val() == '') {
-       	 $('.field').eq(0).append(arr[0])};
-       if ($takeHint.eq(1).val() == '') {
-         $('.field').eq(1).append(arr[1])};
-       if ($takeHint.eq(2).val() == '') {
-         $('.field').eq(2).append(arr[2])};      
-   }
-   function submitOut() {
-   	   $('.hint').remove();
-   }
+      $('.hint').remove();
+      var title = $('input').eq(0).attr('title');
+      $('.field').eq(0).append('<span class="hint">'+title+'</span>');
+      var title = $('input').eq(1).attr('title');
+      $('.field').eq(1).append('<span class="hint">'+title+'</span>');
+      var title = $('input').eq(2).attr('title');
+      $('.field').eq(2).append('<span class="hint">'+title+'</span>');  
+    }  
 });
