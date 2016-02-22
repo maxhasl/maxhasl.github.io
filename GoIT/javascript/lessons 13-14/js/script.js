809d13ee92bedddcb0f53f6547c9567ba02e8595
Max Bullo <koza4ek90@gmail.com>
2016-02-22 18:35:09 +0200

    updated lessons 13-14

--- GoIT/javascript/lessons 13-14/js/script.js
+++ GoIT/javascript/lessons 13-14/js/script.js
@@ -1,35 +1,41 @@
 'use strick';
 $(function() {
+ 
   var arrayQuestions = [
-  {
-    question: "var result = 5 + ‘5’ - 0;",
-    answerOne: "55",
-    answerTwo: "10",
-    answerThree: "50",
-    answerTrue: "55"
-  },
     {
-    question: "Зачем используется ключевое слово var?",
-    answerOne: "Для выполнения написанного выше кода",
-    answerTwo: "Для вывода результатов программы на экран",
-    answerThree: "Для создания новой переменной",
-    answerTrue: "Для создания новой переменной"
-  },
+      title: 'var result = 5 + ‘5’ - 0;',
+      answers: [
+                '55',
+                '10',
+                '50'
+               ],
+      check: '55'
+    },
     {
-    question: "Чем отличаются циклы while от do while?",
-    answerOne: "независимо от условия цикл do while выполнится хотя бы раз",
-    answerTwo: "в javascript нет цикла do while",
-    answerThree: "независимо от условия цикл while выполнится хотя бы раз",
-    answerTrue: "независимо от условия цикл do while выполнится хотя бы раз"
-  },
+      title: 'Зачем используется ключевое слово var?',
+      answers: [
+                'Для выполнения написанного выше кода',
+                'Для вывода результатов программы на экран',
+                'Для создания новой переменной'
+               ],
+      check: 'Для создания новой переменной'
+    },
+    {
+      title: 'Чем отличаются циклы while от do while?',
+      answers: [
+                'независимо от условия цикл do while выполнится хотя бы раз',
+                'в javascript нет цикла do while',
+                'независимо от условия цикл while выполнится хотя бы раз'
+               ],
+      check: 'независимо от условия цикл do while выполнится хотя бы раз'
+    },
   ];
-  
   var objectQuestions = {
     questions: arrayQuestions
   }
 
-  localStorage.setItem('objectQuestions', JSON.stringify(objectQuestions));
-  var retrievedString = localStorage.getItem('objectQuestions');
+  localStorage.setItem('questions', JSON.stringify(objectQuestions));
+  var retrievedString = localStorage.getItem('questions');
   var retrievedObject = JSON.parse(retrievedString);
   var html = $('#template').html();  
   var content = tmpl(html, retrievedObject);
@@ -51,12 +57,12 @@ $(function() {
   function resultCheckAnswer(){
     var resultTest = 0;
     for (var i = 0; i < arrayQuestions.length; i++) {
-       if (arrayAnswer[i] == arrayQuestions[i].answerTrue) {
-         resultTest += 33.3;
+       if (arrayAnswer[i] == arrayQuestions[i].check) {
+         resultTest += 1;
        };
     };
 
-    $modal = $('<div class="modal"> ' + 'Ваш результат: ' + resultTest + '%' + ' </div>');
+    $modal = $('<div class="modal"> ' + 'Ваш результат: ' + resultTest + ' из ' +arrayQuestions.length+ ' </div>');
     $backModal = $('<div class="back--modal"></div>')
     $backModal.one('click', hideModal);
     $body.append($backModal);
@@ -67,7 +73,8 @@ $(function() {
     $('.checkbox').removeAttr('checked');
     $modal.remove();
     $backModal.remove();
-
+    arrayAnswer = [];
+    resultTest = 0;
   }
   var answer = $('.checkbox');
   var result = $('.submit');
