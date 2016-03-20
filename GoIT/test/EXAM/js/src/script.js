3b1b77b14d23261e28fdb56c46de1c59cbeced38
Max Bullo <koza4ek90@gmail.com>
2016-03-20 20:02:18 +0200

    upd

--- GoIT/test/EXAM/js/src/script.js
+++ GoIT/test/EXAM/js/src/script.js
@@ -22,7 +22,21 @@ $(function() {
   });
 
 //AJAX
+// MASONRY
+  function masonry() {
+    var $container = $('#masonry-container');
+    // Инициализация Масонри, после загрузки изображений
+    $container.imagesLoaded( function() {
+       $container.masonry({
+       itemSelector: '.masonry__item',
+       gutter: 20
+       });
+
+    });
+  };
+
    var queryDefault = "http://api.pixplorer.co.uk/image?&amount=7";
+
    function search (link) {
    	$.getJSON(link,
         function(data){
@@ -33,37 +47,22 @@ $(function() {
         var content = tmpl(html, obj);
         $('#masonry-container').append(content);
         masonry();
+        
     });
    };
 
-   search(queryDefault);   
   
    $('.find__link--2').on('click', function (e){
-	  e.preventDefault();
-	  var oldItems = $('#masonry-container').find('.masonry__item');
-    oldItems.remove();
+       e.preventDefault();
+       var oldItems = $('#masonry-container').find('.masonry__item');
+       oldItems.remove();
+       var word = $('.holiday__input__text').val();
+       $('.holiday__input__text').val("");
+       var link = "http://api.pixplorer.co.uk/image?word="+word+"&amount=7&size=tb"
+       search(link);
 
-    $('#masonry-container').masonry( 'remove', oldItems )
-    // layout remaining item elements
-    .masonry('layout');
-});
-
-	  var word = $('.holiday__input__text').val();
-	  $('.holiday__input__text').val("");
-	  var link = "http://api.pixplorer.co.uk/image?word="+word+"&amount=7&size=tb"
-	  search(link);
-});
-
-//MASONRY
-  function masonry() {
-    var $container = $('#masonry-container');
-    // Инициализация Масонри, после загрузки изображений
-    $container.imagesLoaded( function() {
-       $container.masonry({
-       itemSelector: '.masonry__item',
-       gutter: 20
-       });
     });
-  };
+
+   search(queryDefault);   
 
 });
