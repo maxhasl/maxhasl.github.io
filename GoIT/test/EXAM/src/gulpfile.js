36a9b34258afd6b15505d99bc6e20a44365a7e8f
Max Bullo <koza4ek90@gmail.com>
2016-03-27 21:10:11 +0300

    ud

--- GoIT/test/EXAM/src/gulpfile.js
+++ GoIT/test/EXAM/src/gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('plagins', function() {
-  return gulp.src(['js/partials/jquery1.9.1.min.js','js/partials/jquery.xdomainrequest.js','js/partials/jcarousel.js','js/partials/template.js'])
+  return gulp.src(['js/partials/jcarousel.js','js/partials/template.js','js/partials/masonry.pkgd.min.js'])
     .pipe(concat('plagins.js'))
     .pipe(gulp.dest('js/'));
 });
