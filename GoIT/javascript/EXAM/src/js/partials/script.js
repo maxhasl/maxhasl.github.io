dc5aca82bcad3fe1bfe0655910ff1b4681d0b4fe
Max Bullo <koza4ek90@gmail.com>
2016-04-02 13:38:02 +0300

    updated exam

--- GoIT/javascript/EXAM/src/js/partials/script.js
+++ GoIT/javascript/EXAM/src/js/partials/script.js
@@ -34,7 +34,41 @@ $(function() {
     });
   };
 
-   var queryDefault = "http://api.pixplorer.co.uk/image?&amount=7&size=s";
+   var queryDefault = "http://api.pixplorer.co.uk/image?&amount=1&size=s";
+   var parent;
+   var linkArray = [];
+   var objDefault = {
+    data: {
+           images: []
+          }
+   };
+   var imagesDefault = objDefault.data.images;
+  function searchDefault(link){
+    for (var i = 0; i < 7; i++) {
+        
+      $.ajax({ 
+        type: 'GET',
+        url: link,
+        dataType: 'json',
+        cache: false,
+        success: function(data){ 
+                                var obj = {
+                                  data: data
+                                };                                
+                                linkArray.push(data.images[0]);
+                                if(linkArray.length == 7) {
+                                   objDefault.data.images = linkArray;
+                                   renderList(objDefault);
+                                };
+                                
+                               }
+       });
+
+      
+    };//for
+  };
+  
+
 
    function search(link) {
     
@@ -47,15 +81,19 @@ $(function() {
                                 var obj = {
                                   data: data
                                 };
-                                $('#start__container').after('<section id="masonry-container" class="clearfix"></section>');
-                                var html = $('#images-template').html();
-                                var content = tmpl(html, obj);
-                                setTimeout(function(){
-                                $('#masonry-container').append(content);
-                                masonry();
-                                }, 300);
+                                renderList(obj);
                                }
     })
+  };
+  
+  function renderList(obj) {
+    $('#start__container').after('<section id="masonry-container" class="clearfix"></section>');
+    var html = $('#images-template').html();
+    var content = tmpl(html, obj);
+    setTimeout(function(){
+    $('#masonry-container').append(content);
+    masonry();
+    }, 300);
   };
 
   
@@ -67,26 +105,17 @@ $(function() {
        $('.holiday__input__text').val("");
        var link = "http://api.pixplorer.co.uk/image?word="+word+"&amount=7&size=s"
        search(link);
-       setTimeout(take, 2000);
+       setTimeout(take, 3000);
     });
 
-   search(queryDefault);   
-//MODAL
-    var oldIE;
-       if ($('html').is('.lt-ie7, .lt-ie8, .lt-ie9')) {
-          oldIE = true;
-       }
 
-       if (oldIE) {
-        return 0;
-       } else {
-        function createModal(link) {
+
+//MODAL
+   function createModal(link) {
     var $body = $('body');
     var $modal = $('<div class="modal--back"><img class="modal" src="'+link+'"></div>');
-    // var $backModal = $('<div class="modal--back"></div>');
-    // $body.append($backModal);
     $body.append($modal);
-    // $backModal.on('click', hideModal);
+    $('.modal--back').on('click', hideModal);
   };
 
   function hideModal(){
@@ -94,16 +123,25 @@ $(function() {
     $('.modal--back').remove();
   }
 
-  setTimeout(take, 2000);
   function take(){
-    $('.masonry__image').on('click', function(){
-          var image = $(this);
+    $('.masonry__shadow').hover(function(){
+       var title = $(this).parent().find('.title--image');
+       title.css('z-index', '-5');
+    }, function(){
+       var title = $(this).parent().find('.title--image');
+       title.css('z-index', '1');
+    });
+
+
+    $('.masonry__shadow').on('click', function(){
+          var image = $(this).parent().find('.masonry__image');
+          console.log(image);
           var link = image[0].currentSrc;
           createModal(link);
     })
   };
-       }
-
   
+  searchDefault(queryDefault); 
+  setTimeout(take, 3000);
 
 });
