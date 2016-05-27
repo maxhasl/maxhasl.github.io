841629906779f54970a6573fc0773d3bca4d39ce
maxhasl <maxhasl@ukr.net>
2016-05-27 17:43:29 +0300

    upd

---
+++ my works/orangerea/build/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+	<title>Ponchiki</title>
+  <link rel="shortcut icon" href="img/icon.ico" type="image/png">
+  <link rel="stylesheet" href="styles/style.main.min.css">
+</head>
+<body>
