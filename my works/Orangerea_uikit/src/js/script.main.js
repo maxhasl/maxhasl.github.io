2d0f944be3111c4dd4e9f36a643ce2c6e241d33c
maxhasl <maxhasl@ukr.net>
2016-05-30 16:48:28 +0300

    added uikit

---
+++ my works/Orangerea_uikit/src/js/script.main.js
@@ -0,0 +1,12 @@
+"use strict"
+$(function (){
+  $(".input-text").focusout(function(){
+    var textValue = $(".input-text").val();
+    console.log(textValue);
+    if(textValue.indexOf('@')+1) {
+      $(".input-text").css({borderBottom: "1px solid gainsboro"});
+    } else {
+      $(".input-text").css({borderBottom: "1px solid #ff6377"});
+    };
+  });
+});
