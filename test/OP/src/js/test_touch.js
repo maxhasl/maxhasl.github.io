8994f9d769e53271579df07ac7f13335c431ba97
max <maxhasl@ukr.net>
2017-04-27 23:35:08 +0300

    cleaned repository

--- test/OP/src/js/test_touch.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-  var is_touch_device = 'ontouchstart' in document.documentElement;
-  if(is_touch_device) document.write('<link rel="stylesheet" href="styles/style.touch.main.min.css">');
--- test/touch/script.js
+++
@@ -1,3 +0,0 @@
-var is_touch_device = 'ontouchstart' in document.documentElement;
-if(is_touch_device) document.write('<link rel="stylesheet" href="style-touch.css">');
-else document.write('<link rel="stylesheet" href="style.css">');
